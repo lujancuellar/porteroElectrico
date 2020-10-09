d405c742ffebef68e35a75f7221cea7fd00c474c
lujancuellar <67484949+lujancuellar@users.noreply.github.com>
2020-10-09 13:28:27 -0300

    cambios

---
+++ portero.js
@@ -0,0 +1,8 @@
+function primerNumero() {
+    document.getElementById("primerVisor").innerHTML = 1;
+}
+
+function segundoNumero() {
+    let num2= 2;
+    document.getElementById("primerVisor").innerHTML = num2;
+}

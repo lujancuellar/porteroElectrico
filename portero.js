6451314c7089322cdb3591daf379f76f6bc4c22e
lujancuellar <67484949+lujancuellar@users.noreply.github.com>
2020-10-12 12:08:13 -0300

    Cambie botones y ahora puedo borrar

--- portero.js
+++ portero.js
@@ -1,8 +1,16 @@
-function primerNumero() {
-    document.getElementById("primerVisor").innerHTML = 1;
+function presionarBoton(button) {
+    let numero = button.value;
+    document.getElementById("visor1").innerHTML += numero;
+    document.getElementById("visor2").innerHTML += numero;
 }
 
-function segundoNumero() {
-    let num2= 2;
-    document.getElementById("primerVisor").innerHTML = num2;
+function llamarAlDepto() {
+    let visor1 = document.getElementById('visor1');
+    let visor2 = document.getElementById('visor2');
+
+}
+
+function limpiarVisores() {
+    document.getElementById("visor1").innerHTML = "";
+    document.getElementById("visor2").innerHTML = "";
 }

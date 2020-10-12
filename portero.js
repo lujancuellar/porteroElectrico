4d68e0520cde77768db6121bbf4c7d9e47bfcac5
lujancuellar <67484949+lujancuellar@users.noreply.github.com>
2020-10-12 12:51:21 -0300

    Correcin de errores

--- portero.js
+++ portero.js
@@ -4,10 +4,8 @@ function presionarBoton(button) {
     document.getElementById("visor2").innerHTML += numero;
 }
 
-function llamarAlDepto() {
-    let visor1 = document.getElementById('visor1');
-    let visor2 = document.getElementById('visor2');
-
+function llamarDepto() {
+    
 }
 
 function limpiarVisores() {

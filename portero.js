cbb0b72a30d09b679a8eaf963d7cade1a5862827
lujancuellar <67484949+lujancuellar@users.noreply.github.com>
2020-10-14 15:52:52 -0300

    Añado mas css

--- portero.js
+++ portero.js
@@ -14,9 +14,9 @@ function llamarDepto() {
    let depto = document.getElementById("depto").value;
 
    if (piso < 48 && depto < 6) {
-       document.getElementById("llamando").innerHTML = "Lllamando al: " + "piso " + piso + " y departamento " + depto;
+       document.getElementById("llamando").innerHTML = "Llamando al: " + "piso " + piso + " y departamento " + depto;
    } else {
-    document.getElementById("llamando").innerHTML = "Departamento o piso invalido";
+    document.getElementById("llamando").innerHTML = "Departamento o piso inválido";
    }
 }
 
